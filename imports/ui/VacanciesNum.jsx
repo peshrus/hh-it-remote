b4ce09516e8e445e74ed312ee7803147e70cfed6
peshrus@gmail.com <bitbucket16Pass>
2017-05-17 18:45:22 +0300

    Displaying od selected specialization has been added

--- imports/ui/VacanciesNum.jsx
+++ imports/ui/VacanciesNum.jsx
@@ -1,10 +1,11 @@
-/* eslint-disable react/prefer-stateless-function,react/require-default-props,no-undef */
+/* eslint-disable react/prefer-stateless-function,react/require-default-props,no-undef,react/forbid-prop-types */
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Specializations } from '../api/specializations.js';
 
-import { Vacancies, makeCollectionQuery } from '../api/vacancies.js';
+import { makeCollectionQuery, Vacancies } from '../api/vacancies.js';
 
 class VacanciesNum extends Component {
   render() {
@@ -17,7 +18,11 @@ class VacanciesNum extends Component {
         title="Показать найденные вакансии на сайте hh.ru"
       >hh.ru</a>:<span
         className="label label-success"
-      >{this.props.count}</span></p>
+      >{this.props.count}</span>{this.props.specialization ?
+        <span
+          className="label label-primary"
+          title={this.props.specialization.name}
+        >{this.props.specialization.name}</span> : ''}</p>
     );
   }
 }
@@ -25,10 +30,12 @@ class VacanciesNum extends Component {
 VacanciesNum.propTypes = {
   count: PropTypes.number.isRequired,
   hhLink: PropTypes.string,
+  specialization: PropTypes.object,
 };
 
 export default createContainer(() => {
   Meteor.subscribe('vacancies');
+  Meteor.subscribe('specializations');
 
   Meteor.call('getHhLink', (error, result) => {
     if (!error) {
@@ -40,6 +47,8 @@ export default createContainer(() => {
 
   return {
     count: Vacancies.find(makeCollectionQuery()).count(),
-    hhLink: Session.get('hhLink'),
+    // eslint-disable-next-line max-len
+    hhLink: Session.get('hhLink') ? Session.get('hhLink').replace(/((?:&specialization=[0-9.]+)+)/g, FlowRouter.getParam('specId') ? `&specialization=${FlowRouter.getParam('specId')}` : '$1') : '',
+    specialization: Specializations.findOne({ _id: FlowRouter.getParam('specId') }),
   };
 }, VacanciesNum);
